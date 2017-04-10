f183ac6ac803e3cad991dfaaea4073b5ce4d3b65
Justyna Foster <justynawfoster@gmail.com>
2017-04-09 18:15:20 -0600

    functionality init

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-'use strict'
 
 $(document).ready(function(){
-     $('.parallax').parallax();
-   });
+'use strict';
+  
+ $('.parallax').parallax();
+
+var menu = {
+  "Arugula Pie": 11.99,
+  "Royale with Cheese": 8.99,
+  "Smoked Swine": 14.99,
+  "Ice Cream Biscut": 7.99
+};
+
+
+
+
+
+
